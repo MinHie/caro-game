2509aef1741dcdd7d1239ec0aaf987959df77690
hyhy <hynmss170811@fpt.edu.vn>
2023-06-20 18:32:17 +0700

    delete

--- player.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Player:
-    def __init__(self, number, symbol):
-        self.number = number
-        self.symbol = symbol
-        
-    def select_move(self):
-        pass
